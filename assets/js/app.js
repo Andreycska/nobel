018ff3e5f24a0b9a560e018b1af9af9353b2ad31
Andreycska <pashenkoandrew@gmail.com>
2021-03-31 15:54:56 +0300

    create services

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,122 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){n(1),e.exports=n(2)},function(e,t){console.log("file 1");for(var n=document.querySelectorAll("[data-autoresize]"),o=document.querySelectorAll(".form__input"),r=0,i=o.length;r<i;r++)o[r].addEventListener("focus",(function(){this.nextElementSibling.classList.add("focus")})),o[r].addEventListener("blur",(function(){0==this.value&&this.nextElementSibling.classList.remove("focus")}));n.forEach((function(e){e.addEventListener("input",(function(e){var t=e.target;t.style.height="35px",t.style.height=t.scrollHeight+"px"}))})),$(".fresco").venobox({numeratio:!0,border:"10px"});var l=$("#introSlider");l.slick({infinite:!0,slidesToShow:1,slidesToScroll:1,arrows:!1,dots:!0,fade:!0,autoplay:!0,autoplaySpeed:4e3,speed:1e3}),$("#introSliderPrev").on("click",(function(){l.slick("slickPrev")})),$("#introSliderNext").on("click",(function(){l.slick("slickNext")}))},function(e,t){console.log("file 2")}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/assets/js/app.js":
+/*!******************************!*\
+  !*** ./src/assets/js/app.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("console.log('file 1');\r\n\r\nconst textArea = document.querySelectorAll('[data-autoresize]');\r\nlet arrInput = document.querySelectorAll('.form__input');\r\nfor (var i = 0, count = arrInput.length; i < count; i++) {\r\n    arrInput[i].addEventListener(\"focus\", function() {\r\n        this.nextElementSibling.classList.add(\"focus\");\r\n    });\r\n    arrInput[i].addEventListener(\"blur\", function() {\r\n        if (this.value == false) \r\n        this.nextElementSibling.classList.remove(\"focus\");\r\n    });\r\n}\r\ntextArea.forEach(item => {\r\n    item.addEventListener('input', event =>{\r\n        let $this = event.target;\r\n        $this.style.height = '35px'\r\n        $this.style.height = $this.scrollHeight + 'px';\r\n    });\r\n});\r\n\r\n$(\"[data-collapse]\").on(\"click\", function(event) {\r\n    event.preventDefault();\r\n    let blockId = $(this).data(\"collapse\");\r\n    $(this).toggleClass(\"active\");\r\n    // $(blockId).slideToggle();\r\n});\r\n\r\n$('.acordion__content').on('click', function(event) {\r\n    event.stopPropagation();\r\n});\r\n\r\n$('.fresco').venobox ({\r\n    numeratio: true,\r\n    border: '10px'\r\n});\r\n\r\nlet introSlider = $('#introSlider');\r\n\r\nintroSlider.slick({\r\n    infinite: true,\r\n    slidesToShow: 1,\r\n    slidesToScroll: 1,\r\n    arrows: false,\r\n    dots: true,\r\n    fade: true,\r\n    autoplay: true,\r\n    autoplaySpeed: 4000,\r\n    speed: 1000\r\n  });\r\n\r\n  $('#introSliderPrev').on('click', function() {\r\n    introSlider.slick('slickPrev');\r\n  });\r\n\r\n  $('#introSliderNext').on('click', function() {\r\n    introSlider.slick('slickNext');\r\n  });\r\n\n\n//# sourceURL=webpack:///./src/assets/js/app.js?");
+
+/***/ }),
+
+/***/ "./src/assets/js/file2.js":
+/*!********************************!*\
+  !*** ./src/assets/js/file2.js ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("console.log('file 2');\r\n\n\n//# sourceURL=webpack:///./src/assets/js/file2.js?");
+
+/***/ }),
+
+/***/ 0:
+/*!*************************************************************!*\
+  !*** multi ./src/assets/js/app.js ./src/assets/js/file2.js ***!
+  \*************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("__webpack_require__(/*! /Volumes/MAC 4/www/nobel/src/assets/js/app.js */\"./src/assets/js/app.js\");\nmodule.exports = __webpack_require__(/*! /Volumes/MAC 4/www/nobel/src/assets/js/file2.js */\"./src/assets/js/file2.js\");\n\n\n//# sourceURL=webpack:///multi_./src/assets/js/app.js_./src/assets/js/file2.js?");
+
+/***/ })
+
+/******/ });
